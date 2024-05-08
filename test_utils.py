8f887ad1cd7a3540fe7aa89f0d8245964c6a666f
MikolajR13 <piotrek.trebeusz@gmail.com>
2024-05-08 14:09:32 +0200

    test123

--- test_utils.py
+++ test_utils.py
@@ -3,7 +3,7 @@ import pytest
 import utils
 @pytest . mark . parametrize ("a, b, expected ", [(1 , 2 , 3 ) , (2 , 3 ,
     5 ) , (3 , 4 , 7 ) , (4 , 5 , 9 )])
-
+#powinno działać
 def test_add (a , b , expected ):
     result = utils . add (a , b )
     assert result == expected
